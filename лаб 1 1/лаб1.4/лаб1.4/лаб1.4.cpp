e6ad0c3df2624051f6082c81706923c5729d573c
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-03 16:16:43 +0300

    Add files via upload

---
+++ лаб 1 1/лаб1.4/лаб1.4/лаб1.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main() {
+    int integerPart;
+    float decimalPart;
+
+    printf("Enter an integer: ");
+    scanf_s("%d", &integerPart);
+    printf("Enter a floating-point number: ");
+    scanf_s("%f", &decimalPart);
+
+    printf("Sum: %.2f\n", integerPart + decimalPart);
+
+    return 0;
+}
