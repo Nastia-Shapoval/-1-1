c9ef2ec4199640ce40bdad1f04624cc9a4dbf4c4
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-03 16:17:13 +0300

    Add files via upload

---
+++ лаб 1 1/лаб1.5/лаб1.5/лаб1.5.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main() {
+    char symbol;
+
+    printf("Enter a character: ");
+    scanf_s("%c", &symbol);
+
+    printf("Next character: %c\n", symbol + 1);
+
+    return 0;
+}
